735049c132d904b46244f663897586a8957705fd
AshishDhyani131 <ashishdhyani100@gmail.com>
2024-06-01 11:49:09 +0530

    added pagination

--- src/components/placesTable.jsx
+++ src/components/placesTable.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 const data = [
   {
     first: "Ashish",
@@ -38,27 +38,55 @@ const data = [
 ];
 const itemsPerPage = 3;
 const PlacesTable = () => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(data.length / itemsPerPage);
+  const currentData = data.slice(
+    (currentPage - 1) * itemsPerPage,
+    currentPage * itemsPerPage
+  );
+  const handleClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
   return (
-    <table className="placesTable">
-      <thead>
-        <tr className="borderBottom">
-          <th>#</th>
-          <th>First</th>
-          <th>Last</th>
-          <th>Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((ele, index) => (
-          <tr className="borderBottom" key={index}>
-            <td>{index + 1}</td>
-            <td>{ele.first}</td>
-            <td>{ele.last}</td>
-            <td>{ele.handle}</td>
+    <>
+      <table className="placesTable">
+        <thead>
+          <tr className="borderBottom">
+            <th>#</th>
+            <th>First</th>
+            <th>Last</th>
+            <th>Handle</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {currentData.map((ele, index) => (
+            <tr className="borderBottom" key={index}>
+              <td>{index + 1}</td>
+              <td>{ele.first}</td>
+              <td>{ele.last}</td>
+              <td>{ele.handle}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <div className="pagination">
+        <div className="">
+          {Array.from({ length: totalPages }, (_, index) => (
+            <button
+              key={index + 1}
+              onClick={() => handleClick(index + 1)}
+              className={currentPage === index + 1 ? "active" : ""}
+            >
+              {index + 1}
+            </button>
+          ))}
+        </div>
+        <form id="searchNo" className="searchBar">
+          <input type="number" />
+          <button type="button">Search</button>
+        </form>
+      </div>
+    </>
   );
 };
 
